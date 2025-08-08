fb05f0afd83499c7fddd97c5abc222033b4d0155
Chayon Kumar Das <chayon017das@gmail.com>
2025-08-08 23:03:43 +0600

    productionmode

--- app.py
+++ app.py
@@ -181,8 +181,6 @@ def soil_report():
 def uploaded_file(filename):
     return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000)
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
